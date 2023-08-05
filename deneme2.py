b063f9d831b1be00d70e6f48105d2e57e68a961c
Beste Delen <130577307+bestedelen@users.noreply.github.com>
2023-08-05 15:33:54 +0300

    Add files via upload

---
+++ deneme2.py
@@ -0,0 +1,27 @@
+"""
+maasAli = 12000
+maasAhmet = 15000
+vergi = 0.12
+
+print (maasAli - (maasAli * vergi))
+print (maasAhmet - (maasAhmet * vergi))
+"""
+
+# Aşağıdaki siparişlerin toplam bilgisini yazınız
+""""
+sipariş1 = 120 TL
+siparis2 = 112.2 TL
+siparis3 = 34.98 TL
+
+order1 = 120
+order2 = 112.2
+order3 = 34.98 
+total = order1 + order2 + order3
+print("Total:", total)
+"""
+
+def square(num):
+    return num**2
+result=square(2)
+
+print(result)
